871fbf9f158ced81e0bbaf96e7421d2137ee797e
Uță Robert Constantin <roy_robert_99@yahoo.com>
2024-01-21 16:06:38 +0200

    phonebook
    phonebook

---
+++ src/components/Filter/Filter.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './Filter.module.css'
+import PropTypes from 'prop-types'; // ES6
+
+const Filter = ({ value, onChange }) => (
+  <label>
+    Filter by name
+    <input className={styles.FilterInput} type="text" value={value} onChange={onChange} />
+  </label>
+);
+
+Filter.propTypes  = {
+    value: PropTypes.string,
+    onChange: PropTypes.func.isRequired,
+}
+export default Filter;
